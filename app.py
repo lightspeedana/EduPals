2187dc2199476accb0f4231c66e3b5ce97ed14f2
JoshThanki <joshthanki@gmail.com>
2024-04-06 12:01:22 +0100

    added login and register pages

--- app.py
+++ app.py
@@ -6,5 +6,27 @@ app = Flask(__name__)
 def hello():
     return render_template('homepage.html')
 
+
+@app.route('/tasks')
+def tasks():
+    return render_template('tasks.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/login_action', methods = ['POST'])
+def login_action():
+    return True
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/register_action', methods = ['POST'])
+def register_action():
+    return True
+
 if __name__ == '__main__':
     app.run(debug=True)
